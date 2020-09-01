255ef9ca738be2a543203f389cc41cf0f9dec0c7
christopherayork <christopherayork1992@gmail.com>
2020-09-01 18:10:25 -0400

    Added some wrapper movement functions for Tangibles

---
+++ definitions/Area.go
@@ -0,0 +1,6 @@
+package definitions
+
+type Area struct {
+	Tangible
+
+}
--- definitions/Tangible.go
+++ definitions/Tangible.go
@@ -5,5 +5,40 @@ type Tangible struct {
 	health Stat
 	durability Stat
 	description string
+	contents []*Tangible
 }
+
+func (t Tangible) Enter(target Tangible) bool {
+	// define some rules for movement procedures
+	// this will check if movement is permitted into this tangible
+	return false // lets just say false for now
+}
+
+
+func (t Tangible) Entered(target Tangible) {
+	// if Enter() is validated as truthy, this will be called to complete the process and add addition room for behavior
+	// this will return nothing as of now
+}
+
+func (t Tangible) Exit(target Tangible) bool {
+	// called to permit exiting
+	return false // no allowance for now
+}
+
+func (t Tangible) Exited(target Tangible) {
+	// called on confirmation of Exit() and to complete the process plus add room for additional behavior
+
+}
+
+// perhaps the Move method should be migrated to a movable subsection of Tangible
+func (t Tangible) Move(newloc Tangible) bool {
+	// go through the process of checking whether the new target tangible and it's containers will allow this tangible to move into it
+	return false // no for now
+}
+
+// maybe later add some collision checking
+
+
+
+// base type that /area, /region, /zone, /tile, /plot, /obj, and /mob will derive from
 
