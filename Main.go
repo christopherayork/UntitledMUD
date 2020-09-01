a75fc16c56378c28d7d07017d5d257ece3e636f6
christopherayork <christopherayork1992@gmail.com>
2020-09-01 18:10:57 -0400

    Fixed typo in Main.go

--- Main.go
+++ Main.go
@@ -6,7 +6,7 @@ func main() {
 	// do any other things like world loading
 	// as the scope expands, player client and server would be seperated into different applications
 	game := true
-	for game { game := GameLoop() }
+	for game { game = GameLoop() }
 	// we don't want to automatically exit the program just because the game loop ended
 	// we might want to auto-reconnect on connection drop
 	// or we might want to start a new game by returning true on the loop
