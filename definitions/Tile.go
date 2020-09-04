789e03c031a6058d4666c8671ce3f75bc474ee86
christopherayork <christopherayork1992@gmail.com>
2020-09-04 16:31:52 -0400

    Added some variadic functionality to NewTile() to support a map of Tile pointers for immediate assignment of linked tiles and their directions. Also added a SetD

--- definitions/Tile.go
+++ definitions/Tile.go
@@ -9,13 +9,19 @@ This goes all the way up the chain so that If the player Exits a plot and Enters
 Is called on the Individual.
 If they leave an Area and enter a new one that isn't of the same Zone, Zone.Enter() gets called on the individual
 Leaving a Zone and entering a new one in a different Region calls Region.Enter() on the individual as well.
- */
+*/
 
 type Tile struct {
 	Tangible
+	north *Tile
+	south *Tile
+	east *Tile
+	west *Tile
 }
 
-func NewTile(g Gridded, x, y int) (*Tile, error) {
+// Creates a new Tile{} and returns it if it can successfully Enter() a parent Plot{}.
+// The dirs parameter is optional and takes a map expected to hold *Tile pointers with the options of n, s, e, w for keys.
+func NewTile(g Gridded, x, y int, dirs ...map[string]*Tile) (*Tile, error) {
 	if tan, ok := g.(Plot); ok {
 		tile := Tile{}
 		// call g.Enter(tile and get permission back before setting tile.loc)
@@ -23,6 +29,13 @@ func NewTile(g Gridded, x, y int) (*Tile, error) {
 		tile.x = x
 		tile.y = y
 		tile.loc = tan
+		for i := range dirs {
+			if i > 0 { break }
+			if v, ok := dirs[i]["n"]; ok { tile.north = v }
+			if v, ok := dirs[i]["s"]; ok { tile.south = v }
+			if v, ok := dirs[i]["e"]; ok { tile.east = v }
+			if v, ok := dirs[i]["w"]; ok { tile.west = v }
+		}
 		return &tile, nil
 	} else {
 		return nil, errors.New("error: NewTile(), argument for parameter g required to be type Plot")
@@ -51,6 +64,17 @@ func removeInd(inds []*Individual, ind *Individual) (bool, []*Individual) {
 		}
 	}
 	return false, inds
+}
+
+func (t Tile) SetDirection(d string, v *Tile) bool {
+	switch d {
+		case "n": t.north = v
+		case "s": t.south = v
+		case "e": t.east = v
+		case "w": t.west = v
+		default: return false
+	}
+	return true
 }
 
 // tiles don't need x or y, they are single points on the map
