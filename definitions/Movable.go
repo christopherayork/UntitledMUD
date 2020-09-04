ae2d06fa2ba7cbc80634078af2b2fa6dae689167
christopherayork <christopherayork1992@gmail.com>
2020-09-04 17:50:51 -0400

    Added functionality for Move() from type Movable

--- definitions/Movable.go
+++ definitions/Movable.go
@@ -4,11 +4,26 @@ type Movable struct {
 	Tangible
 	health Stat
 	durability Stat
+	loc *Tile
 }
 
 func (m Movable) Move(dir string) bool {
-
-	return true // we'll figure this out later!
+	var next *Tile
+	switch dir {
+	case "n": next = m.loc.north
+	case "s": next = m.loc.south
+	case "e": next = m.loc.east
+	case "w": next = m.loc.west
+	default: return false
+	}
+	if next != nil {
+		canExit := m.loc.Exit(m)
+		if !canExit { return false }
+		success := next.Enter(m)
+		if !success { return false }
+		return true
+	}
+	return false // we'll figure this out later!
 }
 
 type Individual interface {
