73efb09d82f4dc574195b379d83e043793d6cb7e
christopherayork <christopherayork1992@gmail.com>
2020-09-17 18:58:58 -0400

    Multiple refactors, increasing performance, reducing redundancies, and simplifying processes.

--- definitions/MapGenerator.go
+++ definitions/MapGenerator.go
@@ -3,10 +3,7 @@ package definitions
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strconv"
-	"strings"
 )
 
 // Generates a Map{} with designated params
@@ -28,74 +25,34 @@ func (m MapGenerator) Generate() (*Grid, bool) {
 	errjs := ReadMapJSON("map1.json", &mapData)
 	if errjs != nil { fmt.Println(errjs) }
 	// we need a callback for each, since they return a different type in the same format
-	rcb := func(g Grid, x, y int, coords [][]int) { _, _ = NewRegion(g, x, y, coords) }
-	zcb := func(g Grid, x, y int, coords [][]int) { _, _ = NewZone(g, x, y, coords) }
-	acb := func(g Grid, x, y int, coords [][]int) { _, _ = NewArea(g, x, y, coords) }
-	pcb := func(g Grid, x, y int, coords [][]int) { _, _ = NewPlot(g, x, y, coords) }
-	tcb := func(g Grid, x, y int, name, desc string) { _, _ = NewTile(g, x, y, name, desc) }
-	mapData.Regions.MakeGroup(NGrid, rcb)
-	mapData.Zones.MakeGroup(NGrid, zcb)
-	mapData.Areas.MakeGroup(NGrid, acb)
-	mapData.Plots.MakeGroup(NGrid, pcb)
-	mapData.Tiles.MakeGroup(NGrid, tcb, mapData.Legend)
+	rcb := func(x, y int, coords [][]int) { _, _ = NewRegion(*NGrid, x, y, coords) }
+	zcb := func(x, y int, coords [][]int) { _, _ = NewZone(*NGrid, x, y, coords) }
+	acb := func(x, y int, coords [][]int) { _, _ = NewArea(*NGrid, x, y, coords) }
+	pcb := func(x, y int, coords [][]int) { _, _ = NewPlot(*NGrid, x, y, coords) }
+	tcb := func(x, y int, name, desc string) { _, _ = NewTile(*NGrid, x, y, name, desc) }
+	mapData.Regions.MakeGroup(rcb)
+	mapData.Zones.MakeGroup(zcb)
+	mapData.Areas.MakeGroup(acb)
+	mapData.Plots.MakeGroup(pcb)
+	mapData.Tiles.MakeGroup(tcb, mapData.Legend)
 	fmt.Println("Map loaded successfully!")
 	return NGrid, true
 }
 
-// unloads Unmarshal'd JSON into the result pointer (map of interfaces).
-// no return here, just hang onto your result pointer
-func ReadJSON(fileName string, result *map[string]interface{}) error {
-	jsonFile, err := os.Open(fileName)
-	if err != nil {
-		return fmt.Errorf("error: ReadJSON(), cannot open that file")
-	}
-	defer func() {
-		err4 := jsonFile.Close()
-		if err4 != nil { fmt.Println(fmt.Errorf("error: ReadJSON(), couldn't close that file")) }
-	}()
-	byteValue, err2 := ioutil.ReadAll(jsonFile)
-	if err2 != nil {
-		return fmt.Errorf("error: ReadJSON(), cannot read jsonFile into byteValue array")
-	}
-	err3 := json.Unmarshal([]byte(byteValue), result)
-	if err3 != nil {
-		return fmt.Errorf("error: ReadJSON(), json.Unmarshal() failed to map binary to result map")
-	}
-	return nil
-}
-
-func ReadMapJSON(fileName string, result *Mapfile) error {
+func ReadMapJSON(fileName string, result *Mapfile)  (err error) {
 	jsonFile, err := os.Open(fileName)
 	if err != nil {
 		return fmt.Errorf("error: ReadMapJSON(), cannot open that file")
 	}
 	defer func() {
 		err4 := jsonFile.Close()
-		if err4 != nil { fmt.Println(fmt.Errorf("error: ReadMapJSON(), couldn't close that file")) }
+		if err4 != nil { err = fmt.Errorf("error: ReadMapJSON(), couldn't close that file") }
 	}()
-	byteValue, err2 := ioutil.ReadAll(jsonFile)
-	if err2 != nil {
-		return fmt.Errorf("error: ReadMapJSON(), cannot read jsonFile into byteValue array")
-	}
-	err3 := json.Unmarshal([]byte(byteValue), result)
-	if err3 != nil {
+	err = json.NewDecoder(jsonFile).Decode(result)
+	if err != nil {
 		return fmt.Errorf("error: ReadMapJSON(), json.Unmarshal() failed to map binary to result map")
 	}
 	return nil
-}
-
-func GetCoords(merged string) (int, int) {
-	arr := strings.Split(merged, ",")
-	if len(arr) == 1 {
-		firstVal, _ := strconv.Atoi(arr[0])
-		return firstVal, 0
-	} else if len(arr) < 1 {
-		return 0, 0
-	} else {
-		firstVal, _ := strconv.Atoi(arr[0])
-		secondVal, _ := strconv.Atoi(arr[1])
-		return firstVal, secondVal
-	}
 }
 
 func NewMapGen(x, y int) (*Grid, bool) {
@@ -109,18 +66,16 @@ func panicRecover() {
 }
 
 type MapBlock struct {
-	X []int `json:"x"`
-	Y []int `json:"y"`
+	X [2]int `json:"x"`
+	Y [2]int `json:"y"`
 }
 
 func (block MapBlock) SplitX() (int, int) {
-	if len(block.X) >= 2 { return block.X[0], block.X[1] }
-	return 0, 0
+	return block.X[0], block.X[1]
 }
 
 func (block MapBlock) SplitY() (int, int) {
-	if len(block.Y) >= 2 { return block.Y[0], block.Y[1] }
-	return 0, 0
+	return block.Y[0], block.Y[1]
 }
 
 type MapBlockTile struct {
@@ -130,15 +85,15 @@ type MapBlockTile struct {
 
 type MapSectionGridGroup []MapSectionGrid
 
+type GridConstructor func(int, int, [][]int)
+
 // Takes in a *Grid and callback which calls a type constructor.
 // This supplies all the given data for a multi-coordinate Gridded type to be made and expanded over all locations
-func (group MapSectionGridGroup) MakeGroup(g *Grid, callback func(Grid, int, int, [][]int)) bool {
+func (group MapSectionGridGroup) MakeGroup(callback GridConstructor) bool {
 	for _, grid := range group {
-		if len(grid.Block.X) >= 2 && len(grid.Block.Y) >= 2 {
-			pvals := grid.GetPairs()
-			callback(*g, 0, 0, pvals)
-			return true
-		}
+		pvals := grid.GetPairs()
+		callback(0, 0, pvals)
+		return true
 	}
 	return false
 }
@@ -151,7 +106,7 @@ type MapSectionGrid struct {
 func (grid MapSectionGrid) GetPairs() [][]int {
 	xlower, xupper := grid.Block.SplitX()
 	ylower, yupper := grid.Block.SplitY()
-	pvals := make([][]int, 0)
+	pvals := make([][]int, 0, (xupper-xlower)*(yupper-ylower)) // save on reallocations
 	for x := xlower; x <= xupper; x++ {
 		for y := ylower; y <= yupper; y++ {
 			set := []int {x, y}
@@ -163,16 +118,19 @@ func (grid MapSectionGrid) GetPairs() [][]int {
 
 type MapSectionTileGroup []MapSectionTile
 
+type TileConstructor func(int, int, string, string)
+
 // Takes in a *Grid and a callback which holds a constructor for a type.
 // This pulls all the necessary data for a Tile constructor and passes it through
-func (group MapSectionTileGroup) MakeGroup(g *Grid, callback func(Grid, int, int, string, string), legend MapLegend) bool {
+func (group MapSectionTileGroup) MakeGroup(callback TileConstructor, legend MapLegend) bool {
+	success := false
 	for _, tile := range group {
 		descobj := legend.Get(tile.Desc)
 		name, desc := descobj.Name, descobj.Description
-		callback(*g, tile.Block.X, tile.Block.Y, name, desc)
-		return true
+		callback(tile.Block.X, tile.Block.Y, name, desc)
+		success = true
 	}
-	return false
+	return success
 }
 
 type MapSectionTile struct {
